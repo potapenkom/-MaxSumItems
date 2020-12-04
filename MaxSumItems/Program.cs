42d8272c148205c72d3cf7dd749f22c2f913af3d
Misha Potapenko <potapenko05121985@gmail.com>
2020-12-05 00:41:49 +0200

    first commit

---
+++ MaxSumItems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxSumItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
